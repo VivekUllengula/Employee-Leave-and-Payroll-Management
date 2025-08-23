a64d7b4a875297f4a72a3f0e6f2538a4d42288ad
VivekUllengula <vivek.ullengula@thezennialpro.com>
2025-08-23 13:22:29 +0530

    Added the confiuration file for the DB and JWT Auth

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+class Settings(BaseSettings):
+    MONGODB_URI: str = Field(..., description="MongoDB connection string")
+    MONGODB_DB: str = Field(..., description="MongoDB database name")
+
+    JWT_SECRET = Field(..., description="JWT signing secret")
+    JWT_ALGORITHM = Field("HS256", description="JWT algorithm")
+    JWT_ACCESS_TOKEN_EXPIRES_MIN  = Field(60, description="Access Token TTL in minutes")
+
+    APP_NAME: str = Field("Employee Leave & Payroll API")
+    APP_ENV: str = Field("dev")
+    APP_VERSION: str = Field("1.0.0")
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
